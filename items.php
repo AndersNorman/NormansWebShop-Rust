f2a720ac9a4fa0a6a4eeaf15e5675d626002af8d
AndersNorman <36919151+AndersNorman@users.noreply.github.com>
2018-04-13 13:45:08 +0200

    Added functions.
    New functions for items.php

--- items.php
+++ items.php
@@ -9,5 +9,25 @@ $files2[2] = null;
 return $files1;
 }
 
+function getitemjson($shortname){
+	$cdir = scandir("jsonitems",0);
+	$cdir[0] = null;
+	$cdir[1] = null;
+	foreach($cdir as $ser){
+		
+		if($ser != null){
+		
+			$myfile = fopen("jsonitems/".$ser, "r") or die("Unable to open file!");
+		$contnet = fread($myfile,filesize("jsonitems/".$ser));
+		 $obj = json_decode($contnet,true);
+		 if($obj["ShortName"] == $shortname){
+			 
+			 return $contnet;
+		 }
+		
+		}
+	
+}
+}
 
 ?>
